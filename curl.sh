ee8fce0ca51fce796161de77f28b8db5cbd34a99
Poli <raimundophilippe@gmail.com>
2020-11-16 14:21:37 +0100

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-sed -i 's,http://mirror.hetzner.de,http://internal-mirror.poli.network,g' /etc/apt/sources.list.d/hetzner-mirror.list
-
+sed -i s,http://mirror.hetzner.de,http://internal-mirror.poli.network,g /etc/apt/sources.list.d/hetzner-mirror.list
 mv /etc/apt/sources.list.d/hetzner-mirror.list /etc/apt/sources.list.d/polisystems-mirror.list
-
-sleep 2s 
-
-sed -i 's,http://mirror.hetzner.de,http://internal-mirror.poli.network,g' /etc/apt/sources.list.d/polisystems-mirror.list;
-
-sleep 1m 
-
-mv /etc/apt/sources.list.d/hetzner-mirror.list /etc/apt/sources.list.d/polisystems-mirror.list
-
-sed -i 's,http://mirror.hetzner.de,http://internal-mirror.poli.network,g' /etc/apt/sources.list.d/polisystems-mirror.list;
