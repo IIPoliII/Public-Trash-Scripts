985b0a8d0500aa6740396ff1044765f904565946
Poli <raimundophilippe@gmail.com>
2021-01-03 03:14:16 +0100

    Delete curl.sh

--- curl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i s,http://mirror.hetzner.de,http://internal-mirror.poli.network,g /etc/apt/sources.list.d/hetzner-mirror.list
-mv /etc/apt/sources.list.d/hetzner-mirror.list /etc/apt/sources.list.d/polisystems-mirror.list
